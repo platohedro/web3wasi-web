c1561489fc6e2ba61695c2252d9ee1dc5a948236
igor <igor@localhost.localdomain>
2024-07-30 21:19:55 -0500

    ojala

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig,  passthroughImageService } from "astro/config";
+import { defineConfig, } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercelStatic from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
